be610c5d9a71a206c7248db73eea2fe8233d15c3
raquelcps <rrodriguez.codes@gmail.com>
2025-10-05 12:53:26 -0500

    rounds are working again

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,6 +8,17 @@ class PlayersController < ApplicationController
     team_id   = params[:team_id]
     selected_round = (params[:round] || 0).to_i
 
+    # TEMP: Invalidate cache for all rounds while debugging
+    # This works with the og @available_rounds at the bottom
+    # (0..4).each do |round|
+    #   Rails.cache.delete("nba/team_players/#{team_id}/2024-25/#{round}")
+    # end
+    # Rails.cache.delete("nba/team_totals/#{team_id}/2024-25/#{selected_round}")
+
+    # TEMP: Invalidate cache for player rounds while debugging
+    # This works with the 2nd @available_rounds 
+    Rails.cache.delete("nba/player_rounds/#{team_id}/#{player_id}")
+
     # Get team totals for the selected round (so percentages update correctly)
     @team_totals = NbaStatsService.team_totals(team_id: team_id, season: "2024-25", poround: selected_round)
 
@@ -18,10 +29,20 @@ class PlayersController < ApplicationController
     puts @player.inspect
 
     # Collect available rounds (only include if the player actually has stats in that round)
-    @available_rounds = [0, 1, 2, 3, 4].select do |round|
-      stats = NbaStatsService.team_players(team_id: team_id, season: "2024-25", poround: round)
-      stats.any? { |p| p[:player_id].to_s == player_id.to_s }
+
+    # 2nd version
+    @available_rounds = Rails.cache.fetch("nba/player_rounds/#{team_id}/#{player_id}", expires_in: 1.minute) do #was 12.hours
+      [0, 1, 2, 3, 4].select do |round|
+        stats = NbaStatsService.team_players(team_id: team_id, season: "2024-25", poround: round)
+        stats.any? { |p| p[:player_id].to_s == player_id.to_s }
+      end
     end
+
+    # og
+    # @available_rounds = [0, 1, 2, 3, 4].select do |round|
+    #   stats = NbaStatsService.team_players(team_id: team_id, season: "2024-25", poround: round)
+    #   stats.any? { |p| p[:player_id].to_s == player_id.to_s }
+    # end
 
     @selected_round = selected_round
   end  
