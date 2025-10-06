72a527be1597396c2f7107f6e8ac1e984347ee0c
raquelcps <rrodriguez.codes@gmail.com>
2025-10-06 04:17:49 -0500

    define pllayer_contribution, lots of template cleanup, rename loading fn

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,9 @@
 module PlayersHelper
+  def player_contribution(player_stat, team_stat, multiplier: 1)
+    puts "Debugging message in player_contribution:"
+    puts "player_stat: #{player_stat}, team_stat: #{team_stat}, multiplier: #{multiplier}"
+    return 0 if player_stat.nil? || team_stat.nil? || team_stat.to_f.zero?
+
+    ((player_stat.to_f / (team_stat.to_f * multiplier)) * 100).round(2)
+  end
 end
